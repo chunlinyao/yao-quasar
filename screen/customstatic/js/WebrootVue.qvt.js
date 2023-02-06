82095891be8b70c13cfbe0b821980a7f0ea957c4
Yao Chunlin <chunlinyao@gmail.com>
2023-02-06 02:02:51 +0000

    luckysheet的右方向键补丁。

--- screen/customstatic/js/WebrootVue.qvt.js
+++ screen/customstatic/js/WebrootVue.qvt.js
@@ -2603,9 +2603,40 @@ Vue.component('m-luckysheet', {
             }, function() { return !!window.luckysheet; });
         });
         vm.blurhandler = function(){luckysheet.exitEditMode();};
+        function getCaretPosition(editableDiv) {
+            var caretPos = 0,
+              sel, range;
+            if (window.getSelection) {
+              sel = window.getSelection();
+              if (sel.rangeCount) {
+                range = sel.getRangeAt(0);
+                if (range.commonAncestorContainer.parentNode == editableDiv) {
+                  caretPos = range.endOffset;
+                }
+              }
+            } else if (document.selection && document.selection.createRange) {
+              range = document.selection.createRange();
+              if (range.parentElement() == editableDiv) {
+                var tempEl = document.createElement("span");
+                editableDiv.insertBefore(tempEl, editableDiv.firstChild);
+                var tempRange = range.duplicate();
+                tempRange.moveToElementText(tempEl);
+                tempRange.setEndPoint("EndToEnd", range);
+                caretPos = tempRange.text.length;
+              }
+            }
+            return caretPos;
+          }
+        vm.keydownHandler = function(evt){
+            if(evt.key == 'ArrowRight' && getCaretPosition(this) == this.textContent.length) {
+                luckysheet.exitEditMode();
+            }
+        }
         $(document).on('focusout', '.luckysheet-cell-input', vm.blurhandler);
+        //$(document).on('keydown', '.luckysheet-cell-input.editable', vm.keydownHandler);
     },
     beforeDestroy: function() {
         $(document).off('focusout', vm.blurhandler);
+        //$(document).off('keydown', vm.keydownHandler);
     },
 });
